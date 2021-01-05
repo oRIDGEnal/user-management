ea6f6ff4b49c9c662943e1ecaaac71bc51636d89
Siyabonga <siya@incocreative.com>
2021-01-06 00:49:35 +0200

    Initial Commit

---
+++ Areas/Identity/Data/UsersAppUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsersApp.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the UsersAppUser class
+    public class UsersAppUser : IdentityUser
+    {
+        [PersonalData]
+        [Column (TypeName = "nvarchar(50)")]
+        public string FisrtName { get; set; }
+
+        [PersonalData]
+        [Column (TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+
+        [PersonalData]
+        [Column (TypeName = "nvarchar(50)")]
+        public string EmailAdd { get; set; }
+
+        [PersonalData]
+        [Column (TypeName = "nvarchar(7)")]
+        public string Password { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UsersApp.Areas.Identity.Data;
+using UsersApp.Data;
+
+[assembly: HostingStartup(typeof(UsersApp.Areas.Identity.IdentityHostingStartup))]
+namespace UsersApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder) => builder.ConfigureServices((context, services) =>
+        {
+            /*services.AddDbContext<UsersAppDbContext>(options =>
+                options.UseSqlServer(
+                    context.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<UsersAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<UsersAppDbContext>();*/
+        });
+    }
+}
